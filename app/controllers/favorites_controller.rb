89522b20ff2852146f5d15b8d4a170d3c707666d
Emma Converse <emmaconverse@gmail.com>
2018-11-12 14:45:23 -0500

    general add to favorites implemented

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,18 +1,19 @@
 class FavoritesController < ApplicationController
-  before_action :authenticate_admin!
+  before_action :authenticate_user!
     def index
       @favorites = Favorite.all
     end
 
     def create
-      @favorite = Favorite.new(favorites_params)
+      @favorite = Favorite.new(pet_id: params["pet_id"], user: current_user)
       @favorite.save
+      redirect_back(fallback_location: root_path)
     end
 
-private
+    # def add_to_favorites
+    #   tak ethe elements data attr of pet id
+    #   send that id  to api call for that pet-id
+    # end
 
-def favorites_params
-  params.require(:favorite).permit(:user_id, :pet_id)
-end
 
   end
