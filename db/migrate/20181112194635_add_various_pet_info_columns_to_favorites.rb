826c982ebea4a782061bbd2c8f58f14dadf34295
Emma Converse <emmaconverse@gmail.com>
2018-11-15 15:36:13 -0500

    fixed migration with arrays-- favorites

--- db/migrate/20181112194635_add_various_pet_info_columns_to_favorites.rb
+++ db/migrate/20181112194635_add_various_pet_info_columns_to_favorites.rb
@@ -6,13 +6,14 @@ class AddVariousPetInfoColumnsToFavorites < ActiveRecord::Migration[5.2]
     add_column :favorites, :pet_gender, :string
     add_column :favorites, :pet_breed, :string
     add_column :favorites, :pet_description, :string
-    add_column :favorites, :details, :string, array: true, default: '[]'
-    add_column :favorites, :personality, :string, array: true, default: '[]'
     add_column :favorites, :shelter, :string
     add_column :favorites, :phone, :string
     add_column :favorites, :email, :string
     add_column :favorites, :status, :string
     add_column :favorites, :avatar_url, :string
-    add_column :favorites, :photos_urls, :string, array: true, default: '[]'
+    # default '[]' for local doesn't work in postgres for deployment
+    # add_column :favorites, :details, :string, array: true, default: '[]'
+    # add_column :favorites, :personality, :string, array: true, default: '[]'
+    # add_column :favorites, :photos_urls, :string, array: true, default: '[]'
   end
 end
